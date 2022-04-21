d7fe3524a8d5e2c4750e3cb29f39540b4631d82e
gonzabender <gonzabender@gmail.com>
2022-04-21 14:24:12 -0300

    Z

--- src/ar/edu/unq/po2/tp3/Punto.java
+++ src/ar/edu/unq/po2/tp3/Punto.java
@@ -4,7 +4,7 @@ public class Punto {
 	
 	private float x;
 	private float y;
-	
+	private float z;
 	
 	public Punto(float x, float y) {
 		super();
@@ -26,6 +26,13 @@ public class Punto {
 	public void setXY(float x, float y) {
 		this.setX(x);
 		this.setY(y);
+	}
+	public void setZ(float z) {
+		this.z = z;
+	}
+	
+	public float getZ(float z) {
+		return this.z;
 	}
 	
 }
